1b5c6199875868a717494c83afbbd836345e387c
Chris <developer.chris.yun@gmail.com>
2022-11-23 02:14:02 -0500

    Basic routes added and tested

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { GamePageComponent } from './game-page/game-page.component';
+import { ScorePageComponent } from './score-page/score-page.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+
+const routes: Routes = [
+  { path: "game-page", component: GamePageComponent },
+  { path: "", component: WelcomePageComponent },
+  { path: "score-page", component: ScorePageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GamePageComponent } from './game-page/game-page.component';
+import { ScorePageComponent } from './score-page/score-page.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GamePageComponent,
+    ScorePageComponent,
+    WelcomePageComponent
   ],
   imports: [
     BrowserModule,
